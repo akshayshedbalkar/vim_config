a2f50147ba56e79901436e3413a89c5376ea6f2d
Shedbalkar, Akshay <akshay.shedbalkar@efs-auto.de>
2022-06-14 15:11:57 +0200

    bugfix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##############################################################################################
-if [  ! -f setup.sh ] then
+if [  ! -f setup.sh ]; then
     echo "Error: Please execute this script from vim_config directory. Exiting ..."
     exit 0
 fi
@@ -16,29 +16,29 @@ git submodule update --remote --recursive
 
 ##############################################################################################
 echo -e "\nStep 3/6: Creating swapfiles directory ..."
-if [ ! -d swapfiles ] then
+if [ ! -d swapfiles ]; then
     mkdir swapfiles
 fi
 
 ##############################################################################################
 echo -e "\nStep 4/6: Backing up user .vim, vimrc and bashrc to folder .vim_config_backup ..."
-if [ ! -d "$HOME/.vim_config_backup" ] then
+if [ ! -d "$HOME/.vim_config_backup" ]; then
     mkdir "$HOME/.vim_config_backup"
 fi
 
-if [ ! -L "$HOME/.vimrc" ] then
+if [ ! -L "$HOME/.vimrc" ]; then
     mv "$HOME/.vimrc" "$HOME/.vim_config_backup/.vimrc"
 else
     rm "$HOME/.vimrc"
 fi
 
-if [ ! -L "$HOME/.bashrc" ] then
+if [ ! -L "$HOME/.bashrc" ]; then
     mv "$HOME/.bashrc" "$HOME/.vim_config_backup/.bashrc"
 else
     rm "$HOME/.bashrc"
 fi
 
-if [ -d "$HOME/.vim" ] then
+if [ -d "$HOME/.vim" ]; then
     mv "$HOME/.vim" "$HOME/.vim_config_backup/.vim"
 fi
 
