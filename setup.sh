00357361f5f54309792c41f089ba0bfbefafcae7
Shedbalkar, Akshay <akshay.shedbalkar@efs-auto.de>
2022-08-01 13:01:05 +0200

    dotfiles are now a submodule

--- setup.sh
+++ setup.sh
@@ -48,8 +48,8 @@ cp -r "$PWD" "$HOME/.vim"
 
 ##############################################################################################
 echo -e "\nStep 6/6: Updating user .vimrc and .bashrc ..."
-ln -s "$HOME/.vim/.vimrc" "$HOME/.vimrc"
-ln -s "$HOME/.vim/.bashrc" "$HOME/.bashrc"
+ln -s "$HOME/.vim/dotfiles/.vimrc" "$HOME/.vimrc"
+ln -s "$HOME/.vim/dotfiles/.bashrc" "$HOME/.bashrc"
 
 ##############################################################################################
 echo -e "\nDone! You can safely delete the vim_config folder."
